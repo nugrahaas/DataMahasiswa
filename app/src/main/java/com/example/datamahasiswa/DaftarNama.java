f3562236d8bce64a5e2c80de13c68fcfe3c55b40
Nugraha Adi Sulistyo <tyoku1792@gmail.com>
2019-09-11 08:48:38 +0700

    Done

--- app/src/main/java/com/example/datamahasiswa/DaftarNama.java
+++ app/src/main/java/com/example/datamahasiswa/DaftarNama.java
@@ -1,7 +1,9 @@
 package com.example.datamahasiswa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DaftarNama extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DaftarNama extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_nama);
+    }
+
+    public void inputData(View view) {
+        Intent formTambah = new Intent(DaftarNama.this, FormTambah.class);
+        startActivity(formTambah);
     }
 }
--- app/src/main/java/com/example/datamahasiswa/FormTambah.java
+++ app/src/main/java/com/example/datamahasiswa/FormTambah.java
@@ -1,7 +1,9 @@
 package com.example.datamahasiswa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FormTambah extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FormTambah extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_tambah);
+    }
+
+    public void simpanData(View view) {
+        Intent simpanData = new Intent(FormTambah.this, DaftarNama.class);
+        startActivity(simpanData);
     }
 }
