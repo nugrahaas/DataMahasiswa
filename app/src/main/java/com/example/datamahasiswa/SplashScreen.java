ba14b3ca8282403217d3d2650f9a627072c281a2
Nugraha Adi Sulistyo <tyoku1792@gmail.com>
2019-09-11 07:40:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/datamahasiswa/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.datamahasiswa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    private int waktu_loading = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+            }
+        }, waktu_loading);
+    }
+}
